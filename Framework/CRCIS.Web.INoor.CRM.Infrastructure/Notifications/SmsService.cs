6cac84cd3602ef76156cd6314ffa43ab45713b16
Mabdollahi <Mabdollahi@MFASIHI-PC>
2021-11-08 12:08:01 +0330

    sms

--- Framework/CRCIS.Web.INoor.CRM.Infrastructure/Notifications/SmsService.cs
+++ Framework/CRCIS.Web.INoor.CRM.Infrastructure/Notifications/SmsService.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
