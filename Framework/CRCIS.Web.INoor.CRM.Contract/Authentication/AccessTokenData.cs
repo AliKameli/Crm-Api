12679e3cd3d5001b7a2470e02fa9e6a1f0082fc0
Mabdollahi <Mabdollahi@MFASIHI-PC>
2022-02-14 13:51:03 +0330

    permission

--- Framework/CRCIS.Web.INoor.CRM.Contract/Authentication/AccessTokenData.cs
+++ Framework/CRCIS.Web.INoor.CRM.Contract/Authentication/AccessTokenData.cs
@@ -12,7 +12,7 @@ namespace CRCIS.Web.INoor.CRM.Contract.Authentication
         public double ValidToMilliseconds{ get; set; }
         public string AccessToken { get; set; }
         public string RedreshToken { get; set; }
-        public string User { get; set; }
+        //public string User { get; set; }
         public string Action { get; set; }
         public string jsonData { get; set; }
         public string Admin { get; set; }
