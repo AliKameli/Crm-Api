18c64e88372760bdd451b396ed6194f4dee97b65
Mabdollahi <Mabdollahi@MFASIHI-PC>
2022-02-16 12:15:09 +0330

    suggest common answer

--- Domain/CRCIS.Web.INoor.CRM.Domain/Answers/CommonAnswer/Commands/CommonAnswerUpdateCommand.cs
+++ Domain/CRCIS.Web.INoor.CRM.Domain/Answers/CommonAnswer/Commands/CommonAnswerUpdateCommand.cs
@@ -9,15 +9,17 @@ namespace CRCIS.Web.INoor.CRM.Domain.Answers.CommonAnswer.Commands
     public class CommonAnswerUpdateCommand
     {
         public int Id { get; private set; }
-        public string Title { get;private set; }
-        public string AnswerText { get;private set; }
+        public string Title { get; private set; }
+        public string AnswerText { get; private set; }
+        public bool IsActive { get; private set; }
         public int Priority { get; private set; }
 
-        public CommonAnswerUpdateCommand(int id, string title, string answerText, int priority)
+        public CommonAnswerUpdateCommand(int id, string title, string answerText, bool isActive, int priority)
         {
             Id = id;
             Title = title;
             AnswerText = answerText;
+            IsActive = isActive;
             Priority = priority;
         }
     }
--- Presentation/CRCIS.Web.INoor.CRM.WebApi/Models/CommonAnswer/CommonAnswerUpadateModel.cs
+++ Presentation/CRCIS.Web.INoor.CRM.WebApi/Models/CommonAnswer/CommonAnswerUpadateModel.cs
@@ -10,6 +10,7 @@ namespace CRCIS.Web.INoor.CRM.WebApi.Models.CommonAnswer
         public int Id { get; set; }
         public string Title { get; set; }
         public string AnswerText { get; set; }
+        public bool IsActive { get; set; }
         public int Priority { get; set; }
     }
 }
