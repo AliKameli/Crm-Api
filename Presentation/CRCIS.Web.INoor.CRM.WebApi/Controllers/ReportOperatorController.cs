7b46e5c2701bcd4ed19c3b7c51a2e32dfc8a2e63
Mabdollahi <Mabdollahi@MFASIHI-PC>
2022-02-28 12:16:49 +0330

    fix

--- Presentation/CRCIS.Web.INoor.CRM.WebApi/Controllers/ReportOperatorController.cs
+++ Presentation/CRCIS.Web.INoor.CRM.WebApi/Controllers/ReportOperatorController.cs
@@ -1,30 +1,30 @@
-﻿using CRCIS.Web.INoor.CRM.Contract.Repositories.Reports;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using CRCIS.Web.INoor.CRM.Contract.Repositories.Reports;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace CRCIS.Web.INoor.CRM.WebApi.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ReportOperatorController : ControllerBase
-    {
-        private readonly IReportRepository _reportRepository;
-        public ReportOperatorController(IReportRepository reportRepository)
-        {
-            _reportRepository = reportRepository;
-        }
+//namespace CRCIS.Web.INoor.CRM.WebApi.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class ReportOperatorController : ControllerBase
+//    {
+//        private readonly IReportRepository _reportRepository;
+//        public ReportOperatorController(IReportRepository reportRepository)
+//        {
+//            _reportRepository = reportRepository;
+//        }
 
-        [HttpGet]
-        [Authorize]
-        public async Task<IActionResult> Get()
-        {
-            //_reportRepository.get
-            return Ok();
-        }
-    }
-}
+//        [HttpGet]
+//        [Authorize]
+//        public async Task<IActionResult> Get()
+//        {
+//            //_reportRepository.get
+//            return Ok();
+//        }
+//    }
+//}
