f40ad5fd16834f7913aee25b91cab46ff2c332ee
Mabdollahi <Mabdollahi@MFASIHI-PC>
2022-02-28 12:15:46 +0330

    report fix

--- Domain/CRCIS.Web.INoor.CRM.Domain/Reports/Case/Dtos/ReportCaseResponseFullDto.cs
+++ Domain/CRCIS.Web.INoor.CRM.Domain/Reports/Case/Dtos/ReportCaseResponseFullDto.cs
@@ -10,6 +10,7 @@ namespace CRCIS.Web.INoor.CRM.Domain.Reports.Case.Dtos
     {
         public long Id { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
         public DateTime CreateDateTime { get; set; }
         public string ProductTitle { get; set; }
         public long RowNumber { get; set; }
@@ -19,7 +20,7 @@ namespace CRCIS.Web.INoor.CRM.Domain.Reports.Case.Dtos
         public int? AdminId { get; set; }
         public string FirstSubject { get; set; }
         public long TotalCount { get; set; }
-
+        public string CreateDateTimePersian { get; set; }
         public bool AllowAssignToMe { get; set; }
         public bool AllowAnswerByMe { get; set; }
         public bool AllowAssignToOther { get; set; }
---
+++ Presentation/CRCIS.Web.INoor.CRM.WebApi/Controllers/ReportOperatorController.cs
@@ -0,0 +1,30 @@
+﻿using CRCIS.Web.INoor.CRM.Contract.Repositories.Reports;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRCIS.Web.INoor.CRM.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportOperatorController : ControllerBase
+    {
+        private readonly IReportRepository _reportRepository;
+        public ReportOperatorController(IReportRepository reportRepository)
+        {
+            _reportRepository = reportRepository;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Get()
+        {
+            //_reportRepository.get
+            return Ok();
+        }
+    }
+}
