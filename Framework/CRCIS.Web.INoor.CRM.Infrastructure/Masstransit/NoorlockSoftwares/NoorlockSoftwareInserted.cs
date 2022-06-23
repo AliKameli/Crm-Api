c60673cbfe9352ef6a7da1769a7888eedbfc6792
Mabdollahi <Mabdollahi@MFASIHI-PC>
2022-06-23 11:03:33 +0430

    fix update secret

--- Framework/CRCIS.Web.INoor.CRM.Infrastructure/Masstransit/NoorlockSoftwares/NoorlockSoftwareInserted.cs
+++ Framework/CRCIS.Web.INoor.CRM.Infrastructure/Masstransit/NoorlockSoftwares/NoorlockSoftwareInserted.cs
@@ -9,6 +9,9 @@ namespace CRCIS.Web.INoor.CRM.Infrastructure.Masstransit.NoorlockSoftwares
     public class NoorlockSoftwareInserted
     {
         public string JsonString { get; set; }
+        public int NoorlockSoftwareCode { get; set; }
+        public string CrmSoftwareSecret { get; set; }
+        public string SotwareName { get; set; }
     }
     public class NoorlockSoftwareInsertedModel
     {
