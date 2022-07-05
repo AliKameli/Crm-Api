9f50da3eedf540356d1fd3e45baa236752750da4
Mabdollahi <Mabdollahi@MFASIHI-PC>
2022-07-05 10:52:29 +0430

    fix logout

---
+++ Presentation/CRCIS.Web.INoor.CRM.WebApi/Controllers/LogoutController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace CRCIS.Web.INoor.CRM.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class LogoutController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public LogoutController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+
+            return Redirect($"{_configuration["VueUrl"]}login"); 
+        }
+    }
+}
