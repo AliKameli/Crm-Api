dcb28f32bbe9eac7437da3a2be0fd4c044a7d44c
mabdollahi <mabdollahi@MFASIHI-PC>
2022-11-24 13:53:46 +0330

    voip infrastrcture

---
+++ Domain/CRCIS.Web.INoor.CRM.Domain/Reports/Person/Queries/PersonByMobileReportQuery.cs
@@ -0,0 +1,50 @@
+﻿using CRCIS.Web.INoor.CRM.Utility.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRCIS.Web.INoor.CRM.Domain.Reports.Person.Queries
+{
+    public class PersonByMobileReportQuery : AbstractDataTableQuery
+    {
+        public string Order { get; private set; }
+        public string Mobile { get; private set; }
+        public PersonByMobileReportQuery(int pageIndex, int pageSize,
+            string sortField, SortOrder? sortOrder, string mobile)
+            : base(pageIndex, pageSize)
+        {
+            sortField = sortField?.Trim();
+            if (!string.IsNullOrEmpty(sortField) && sortField != null)
+            {
+                Order = $"{sortField} {sortOrder.ToString()}";
+            }
+            Mobile = mobile?.Trim();
+            //if (string.IsNullOrEmpty(Mobile) == false)
+            //{
+            //    if (Mobile.StartsWith("+98-9"))
+            //    {
+            //        var temp = Mobile.Substring("+98-9".Length);
+            //        Mobile = Mobile?.Replace("+98-9", "9");
+            //    }
+
+            //    if (Mobile.StartsWith("00989"))
+            //    {
+            //        Mobile = Mobile?.Replace("00989", "9")  ;
+            //    }
+
+            //    if (Mobile.StartsWith("0989"))
+            //    {
+
+            //    }
+            //        Mobile = Mobile?.Replace("0989", "9")      ;
+
+            //    if (Mobile.StartsWith("+98-9"))
+            //        Mobile = Mobile?.Replace("09", "9");
+
+            //}
+
+        }
+    }
+}
---
+++ Presentation/CRCIS.Web.INoor.CRM.WebApi/Controllers/ReportPersonByMobileViopController.cs
@@ -0,0 +1,36 @@
+﻿using CRCIS.Web.INoor.CRM.Contract.ElkSearch;
+using CRCIS.Web.INoor.CRM.Domain.Reports.Person.Queries;
+using CRCIS.Web.INoor.CRM.Domain.Users.UserSearch;
+using CRCIS.Web.INoor.CRM.Utility.Queries;
+using CRCIS.Web.INoor.CRM.Utility.Response;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CRCIS.Web.INoor.CRM.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportPersonByMobileViopController : ControllerBase
+    {
+        private readonly IUserSearch _userSearch;
+
+        public ReportPersonByMobileViopController(IUserSearch userSearch)
+        {
+            _userSearch = userSearch;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] string mobile, [FromQuery] string sortField = null, [FromQuery] SortOrder? sortOrder = null)
+        {
+            var data = await _userSearch.SearchAsync("", mobile, "", "");
+
+            var query = new PersonByMobileReportQuery(1, 99999, sortField, sortOrder, mobile); ;
+
+
+
+            var datatableresponse = new DataTableResponse<List<SeadrchUserDto>>(data.Data, data.TotalCount);
+            return Ok(datatableresponse);
+        }
+    }
+}
